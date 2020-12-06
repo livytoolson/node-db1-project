306f43adee698e9ca34b939caa4c20c3ae4f48e4
Olivia Toolson <livytoolson@gmail.com>
2020-12-06 14:05:39 -0800

    get all accounts

--- api/server.js
+++ api/server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 
 const db = require("../data/dbConfig.js");
-// const accountsRouter = require('./accounts-router');
 
 const server = express();
 
-server.use(express.json());
+server.get('/', (req, res) => {
+    const getAll = db('*').from('accounts').toString();
+    db('accounts')
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ message: ' ' })
+        })
+})
 
-// server.use('/api/accounts', accountsRouter);
+server.get('/:id', (req, res) => {
+    const { id } = req.params
+})
+
+server.post('/', (req, res) => {
+
+})
+
+server.put('/:id', (req, res) => {
+    const { id } = req.params
+})
+
+server.delete('/:id', (req, res) => {
+    const { id } = req.params
+})
+
+server.use(express.json());
 
 module.exports = server;
